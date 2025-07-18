183c72a774496869cf40546b2f34cbed0196b855
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-07-18 02:52:27 +0200

    Remove D969.

--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -22412,46 +22412,6 @@ const machine_t machines[] = {
         .snd_device = &sb_vibra16cl_onboard_device,
         .net_device = NULL
     },
-	
-	{
-        .name = "[i430HX] Siemens Nixdorf Scenic Pro M5 (D969)",
-        .internal_name = "d969",
-        .type = MACHINE_TYPE_SOCKET7,
-        .chipset = MACHINE_CHIPSET_INTEL_430HX,
-        .init = machine_at_d969_init,
-        .p1_handler = NULL,
-        .gpio_handler = NULL,
-        .available_flag = MACHINE_AVAILABLE,
-        .gpio_acpi_handler = NULL,
-        .cpu = {
-            .package = CPU_PKG_SOCKET5_7,
-            .block = CPU_BLOCK_NONE,
-            .min_bus = 50000000,
-            .max_bus = 66666667,
-            .min_voltage = 2800,
-            .max_voltage = 3520,
-            .min_multi = 1.5,
-            .max_multi = 3.0
-        },
-        .bus_flags = MACHINE_PS2_PCI | MACHINE_BUS_USB,
-        .flags = MACHINE_IDE_DUAL | MACHINE_APM | MACHINE_SOUND | MACHINE_VIDEO | MACHINE_GAMEPORT | MACHINE_USB,
-        .ram = {
-            .min = 8192,
-            .max = 131072,
-            .step = 8192
-        },
-        .nvrmask = 511,
-        .kbc_device = NULL,
-        .kbc_p1 = 0xff,
-        .gpio = 0xffffffff,
-        .gpio_acpi = 0xffffffff,
-        .device = NULL,
-        .fdc_device = NULL,
-        .sio_device = NULL,
-        .vid_device = &gd5440_onboard_pci_device,
-        .snd_device = NULL,
-        .net_device = NULL
-    },
     /* Has a SM(S)C FDC37C935 Super I/O chip with on-chip KBC with Phoenix
        MultiKey/42 (version 1.38) KBC firmware. */
     {
