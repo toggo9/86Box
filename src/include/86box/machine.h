dbf1df9e6e3f192378362cd956e172151224b29f
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-08-13 02:33:45 +0200

    Add Compaq "Camaro" machine definition.

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -838,6 +838,7 @@ extern int machine_at_5aa_init(const machine_t *);
 extern int machine_at_5ax_init(const machine_t *);
 
 extern int machine_at_ax59pro_init(const machine_t *);
+extern int machine_at_5114vu_init(const machine_t *);
 extern int machine_at_mvp3_init(const machine_t *);
 extern int machine_at_ficva503a_init(const machine_t *);
 extern int machine_at_5emapro_init(const machine_t *);
