b59e85981e84fd9436848dadc1cac08bf092e36c
toggo9 <121191375+toggo9@users.noreply.github.com>
2025-09-30 10:49:51 +0200

    Fix a compile breaking error.

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -919,6 +919,11 @@ extern int             machine_at_morrison32_hp_init(const machine_t *);
 extern int             machine_at_advt8100p_init(const machine_t *);
 extern int             machine_at_morrison64_init(const machine_t *);
 extern int             machine_at_morrisonmc_init(const machine_t *);
+
+#ifdef EMU_DEVICE_H
+extern const device_t pc330_65x6_device;
+#endif
+
 extern int             machine_at_pc330_65x6_init(const machine_t *);
 extern int             machine_at_powermatev_init(const machine_t *);
 extern int             machine_at_hawk_init(const machine_t *);
